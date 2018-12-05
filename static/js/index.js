766f5e5312398ca6fc60682568d57c5d12963c54
skynet0590 <skynet0590@gmail.com>
2018-12-05 15:00:53 +0700

    change to 8000 port

--- static/js/index.js
+++ static/js/index.js
@@ -46,8 +46,8 @@ function mainController($rootScope, $scope, $mdSidenav, $http) {
     if (window.location.protocol === "https:") {
       ws_proto = "wss:"
     }
-    //
-    var socket = new WebSocket(ws_proto + "//" + window.location.hostname + ":" +  "8080" + "/ws/" + btoa(file));
+    //window.location.port
+    var socket = new WebSocket(ws_proto + "//" + window.location.hostname + ":" +  "8000" + "/ws/" + btoa(file));
     var container = angular.element(document.querySelector("#container"));
 
     // clear the contents
